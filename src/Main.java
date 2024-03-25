c473c2ee73adf51f11cb0a8d789c875eee926ae4
Tikhon <tihonbikbaev@gmail.com>
2024-03-25 17:20:19 +0300

    2.18

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Monstr monstr = new Monstr(2);
+        monstr.voice(10);
+        monstr.voice(10, "soafafsdfsd");
+    }
+}
--- src/Monstr.java
+++ src/Monstr.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Monstr {
+import java.awt.*;
+
+public class Monstr {
+    int eyesCount;
+    int armsCount;
+    int legsCount;
+    int everythingCount;
+    Monstr(){
+        eyesCount = 2;
+        armsCount = 2;
+        legsCount = 2;
+    }
+    Monstr(int everythingCount){
+        eyesCount = everythingCount;
+        armsCount = everythingCount;
+        legsCount = everythingCount;
+    }
+    Monstr(int eyesCount, int armsCount, int legsCount){
+        this.eyesCount = eyesCount;
+        this.armsCount = armsCount;
+        this.legsCount = legsCount;
+    }
+    public void voice(){
+        System.out.println("grrrrrrrrrrrrrr");
+    }
+    public void voice(int count){
+        for (int i = 0; i < count; i++) {
+            System.out.println("grrrrrrrrrrrrrrrr");
+        }
+    }
+    public void voice(int count, String scream){
+        for (int i = 0; i < count; i++) {
+            System.out.println(scream);
+        }
+    }
 }
