b632e36d84bb6ca85009b2311d15649305400e6b
Tikhon <tihonbikbaev@gmail.com>
2024-03-25 19:08:06 +0300

    2.21

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,7 +2,7 @@ package test;
 import things.*;
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("John", 25);
+        System.out.println(MyMath.sum(9829, 2352, 342));
 
     }
 }
---
+++ src/things/MyMath.java
@@ -0,0 +1,27 @@
+package things;
+
+public class MyMath {
+    public static final double PI = 3.14;
+    public static int sum(int... numbers){
+        int sum = 0;
+        for (int number : numbers) {
+            sum+=number;
+        }
+        return sum;
+    }
+    public static double length(double radius){
+        return 2* PI *radius;
+    }
+    public static  double area(double radius){
+        return radius*radius* PI;
+    }
+    public static int multiple(int a, int b){
+     return a*b;
+    }
+    public static double multiple(double a, double b){
+        return a*b;
+    }
+    public static  int multiple(int a){
+        return a*a;
+    }
+}
